09b6e5d0b97d5d80ec476f8d5b6042e7b597bcaf
Sangwon Cho <hyde0401@gmail.com>
2023-12-19 17:42:23 +0900

    practice : function, statement, expression, if

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,34 @@
 fn main() {
+    // practice_variable();     
+    // practice_function(5, 'y');
+    // let statement = (let a = 1) // error
+    // let expr = practice_expression();
+    // practice_branches(3); practice_branches(2); practice_branches(4);
+}
+
+fn practice_branches(num: i32) {
+    if num == 2 {
+        println!("is 2");
+    } else if num == 4 {
+        println!("is 4");
+    } else {
+        println!("is etc");
+    }
+}
+
+fn practice_expression() -> i32 {
+    let x = 1 + 3;
+    // return x; // success
+    x // success (세미 콜론 생략)
+    // x; // error
+}
+
+fn practice_function(x: i32, y: char) {
+    println!("parameter : {x}, {y}");
+
+}
+
+fn practice_variable() {
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; // constant
     let mut x = 5; // mutable variable
     println!("the value of x is : {x}");
@@ -22,5 +52,4 @@ fn main() {
     // impossible
     // let mut spaces = "     "; // String
     // spaces = spaces.len(); // 이미 String type이라 Number type을 담을 수 없음.
-    
 }
