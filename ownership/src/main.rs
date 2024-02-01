0415f70ec4b5af39c19ba32ceb16e5d234ab6989
hy43 <hyde0401@gmail.com>
2024-02-01 23:05:47 +0900

    feat : dangling pointer

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,39 @@
 fn main() {
     // practice_ownership();
     // practice_reference_borrow();
-    practice_mutable_reference();
+    // practice_mutable_reference();
+    // practice_scope_reference();
+    practice_dangling_pointer();
 }
 
-fn practice_mutable_reference(){
+fn practice_dangling_pointer() {
+    // let reference_to_nothing = dangle();
+    let reference_to_nothing = no_dangle();
+    println!("{reference_to_nothing}");
+}
+
+fn no_dangle() -> String {
+    let str = String::from("hello");
+    str
+}
+
+// fn dangle() -> &String {
+//     let str = String::from("hello");
+//     &str
+// }
+
+fn practice_scope_reference() {
+    let mut str = String::from("hello");
+
+    let ref1 = &str;
+    let ref2 = &str;
+    println!("{ref1}, {ref2}");
+
+    let ref3 = &mut str;
+    println!("{ref3}");
+}
+
+fn practice_mutable_reference() {
     let mut s1 = String::from("hello");
     println!("s1 : {}", s1);
     change(&mut s1);
