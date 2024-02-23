1b542e14d43253e979b91cf709601d27e10e6e0d
Sangwon Cho <hyde0401@gmail.com>
2024-02-23 09:57:19 +0900

    practice : structure method

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -1,6 +1,77 @@
 fn main() {
     // struct_basic();   
-    struct_update();
+    // struct_update();
+    rectangles();
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+impl Rectangle {
+    // 첫 매개 변수가 &self 이기 때문에 메소드
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+
+    // 첫 매개 변수가 &self가 아니기 때문에 연관 함수
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size
+        }
+    }
+}
+
+fn rectangles() {
+    //let width1 = 30;
+    // let height1 = 50;
+    // let rect1 = (30, 50);
+    let scale = 2;
+    let rect1 = Rectangle {
+        // width: dbg!(30 * scale),
+        width: 30,
+        height: 50
+    };
+    println!(
+            "The area of the rectangle is {} square pixels.",
+            // area(width1, height1)
+            // area(rect1)
+            // area(&rect1)
+            rect1.area()
+        );
+    println!("rect : {:#?}", rect1);
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45
+    };
+
+    println!("Can rect1 hold rect2 ? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3 ? {}", rect1.can_hold(&rect3));
+
+    // assoiated function
+    let square = Rectangle::square(12);
+    println!("square : {} * {}", square.width, square.height);
+}
+
+// fn area(width: u32, height: u32) -> u32 {
+//  width * height
+// fn area(dimensions: (u32, u32)) -> u32 {
+//    dimensions.0 * dimensions.1
+fn area(rect: &Rectangle) -> u32 {
+    rect.width * rect.height
 }
 
 struct User {
