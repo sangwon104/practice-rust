ec44b7db2e027370a71817bf0b7daa087c9a9675
Sangwon Cho <hyde0401@gmail.com>
2024-02-23 09:58:32 +0900

    practice : enumerations basic

--- enumerations/src/main.rs
+++ enumerations/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    enum_basic();
+}
+
+enum IpAddr {
+    V4(String),
+    V6(String)
+}
+
+fn enumerations() {
+    let home = IpAddr::V4(String::from("127.0.0.1"));
 }
