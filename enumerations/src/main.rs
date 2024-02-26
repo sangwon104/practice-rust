853e67ed035cfe65b2a4620b199984b1d8f81390
Sangwon Cho <hyde0401@gmail.com>
2024-02-26 14:52:53 +0900

    practice : catch all pattern and if let

--- enumerations/src/main.rs
+++ enumerations/src/main.rs
@@ -1,9 +1,28 @@
 fn main() {
     // enum_basic();
     // enum_option();
-    enum_match();
+    // enum_match();
+    // catch_all_pattern();
+    if_let();
 }
 
+fn if_let() {
+    let config_max = Some(3u8);
+
+    // match config_max {
+    //     Some(max) => println!("The maximum is configured to be {}", max),
+    //     _ => ()
+    // }
+    if let Some(max) = config_max {
+         Some(max) => println!("The maximum is configured to be {}", max),
+    }
+
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+}
 #[derive(Debug)]
 enum UsState {
     Alabama,
@@ -20,6 +39,36 @@ enum Coin {
 fn enum_match() {
     println!("penny coin : {}", value_in_cents(Coin::Penny));
     println!("quarter coin : {}", value_in_cents(Coin::Quarter(UsState::Alabama)));
+    // println!("quarter coin : {}", value_in_cents(Coin::Quarter));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
+
+fn catch_all_pattern() {
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        // _ => reroll()
+        _ => () // 어디에도 포함되지 않는 경우, 동작하지 않음을 명시
+    }
+}
+
+fn add_fancy_hat() {
+    println!("add fancy hat");
+}
+
+fn remove_fancy_hat() {
+    println!("remove fancy hat");
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1)
+    }
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
